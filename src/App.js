052452178677dc9704767e16209e7a0ddf641fec
Carlos Hughes <chughe88@kennesaw.edu>
2020-10-16 00:09:22 -0400

    changed photo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import logo from "./logo.svg";
+import logo from "./logo.png";
 import "./App.css";
 import ButtonGroup from "@material-ui/core/ButtonGroup";
 import Checkbox from "@material-ui/core/Checkbox";
